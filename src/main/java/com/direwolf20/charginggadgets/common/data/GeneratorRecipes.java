389ef7b79581047a8d61e704ffb4ac0aa1a7aefb
Michael Hillcox <michaelhillcox@gmail.com>
2019-11-21 17:54:28 +0000

    Registration, gens and textures.

--- src/main/java/com/direwolf20/charginggadgets/common/data/GeneratorRecipes.java
+++ src/main/java/com/direwolf20/charginggadgets/common/data/GeneratorRecipes.java
@@ -1,10 +1,30 @@
 package com.direwolf20.charginggadgets.common.data;
 
+import com.direwolf20.charginggadgets.common.blocks.ModBlocks;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraftforge.common.Tags;
 
-public class GeneratorRecipes extends RecipeProvider {
-    public GeneratorRecipes(DataGenerator gen) {
-        super(gen);
+import java.util.function.Consumer;
+
+public final class GeneratorRecipes extends RecipeProvider {
+    public GeneratorRecipes(DataGenerator generator) {
+        super(generator);
+    }
+
+    @Override
+    protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(ModBlocks.CHARGING_STATION.get())
+                .key('i', Tags.Items.INGOTS_GOLD)
+                .key('r', Tags.Items.DUSTS_REDSTONE)
+                .key('l', Tags.Items.GEMS_LAPIS)
+                .key('d', Tags.Items.GEMS_DIAMOND)
+                .patternLine("iri")
+                .patternLine("drd")
+                .patternLine("ili")
+                .addCriterion("has_diamonds", hasItem(Tags.Items.GEMS_DIAMOND))
+                .build(consumer);
     }
 }
--- src/main/java/com/direwolf20/charginggadgets/common/data/Generators.java
+++ src/main/java/com/direwolf20/charginggadgets/common/data/Generators.java
@@ -1,8 +1,11 @@
 package com.direwolf20.charginggadgets.common.data;
 
 import com.direwolf20.charginggadgets.ChargingGadgets;
+import com.direwolf20.charginggadgets.common.blocks.ChargingStationBlock;
+import com.direwolf20.charginggadgets.common.blocks.ModBlocks;
 import net.minecraft.data.DataGenerator;
-import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.generators.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -12,7 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
  *           expand the more complex or larger generators to a different file.
  */
 @Mod.EventBusSubscriber(modid = ChargingGadgets.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Generators {
+public final class Generators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
@@ -23,6 +26,54 @@ public class Generators {
 
         if( event.includeClient() ) {
             generator.addProvider(new GeneratorLanguage(generator));
+            generator.addProvider(new GeneratorBlockStates(generator, event.getExistingFileHelper()));
+            generator.addProvider(new GeneratorItemModels(generator, event.getExistingFileHelper()));
+        }
+    }
+
+    private static final class GeneratorItemModels extends ItemModelProvider {
+        public GeneratorItemModels(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+            super(generator, ChargingGadgets.MOD_ID, existingFileHelper);
+        }
+
+        @Override
+        protected void registerModels() {
+            String path = ModBlocks.CHARGING_STATION.get().getRegistryName().getPath();
+            getBuilder(path).parent(new ModelFile.UncheckedModelFile(modLoc("block/" + path)));
+        }
+
+        @Override
+        public String getName() {
+            return "Item Models";
+        }
+    }
+
+    private static final class GeneratorBlockStates extends BlockStateProvider {
+        public GeneratorBlockStates(DataGenerator gen, ExistingFileHelper exFileHelper) {
+            super(gen, ChargingGadgets.MOD_ID, exFileHelper);
+        }
+
+        @Override
+        protected void registerStatesAndModels() {
+            ResourceLocation side = modLoc("blocks/charging_station_side");
+            // Sorry for the formatting on this one, it's because we have to define all the sides :(
+            getVariantBuilder(ModBlocks.CHARGING_STATION.get()).partialState().with(ChargingStationBlock.LIT, false).addModels(
+                    ConfiguredModel.builder().modelFile(cube(
+                            ModBlocks.CHARGING_STATION.get().getRegistryName().getPath(),
+                            modLoc("blocks/charging_station_bottom"),
+                            modLoc("blocks/charging_station_top"),
+                            modLoc("blocks/charging_station_frontoff"),
+                            side, side, side
+                    ).texture("particle", side)).build()
+            ).with(ChargingStationBlock.LIT, true).addModels(
+                    ConfiguredModel.builder().modelFile(cube(
+                            ModBlocks.CHARGING_STATION.get().getRegistryName().getPath(),
+                            modLoc("blocks/charging_station_bottom"),
+                            modLoc("blocks/charging_station_top"),
+                            modLoc("blocks/charging_station_fronton"),
+                            side, side, side
+                    ).texture("particle", side)).build()
+            );
         }
     }
 }
