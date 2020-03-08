32aee4bf7f707544d1927c9a95de55ae014efa42
Michael Hillcox <hello.michaelhillcox@gmail.com>
2020-03-08 01:32:56 +0000

    Improved the builder to be less hacky

--- src/main/java/com/direwolf20/charginggadgets/common/tiles/ChargingStationTile.java
+++ src/main/java/com/direwolf20/charginggadgets/common/tiles/ChargingStationTile.java
@@ -4,7 +4,6 @@ import com.direwolf20.charginggadgets.common.blocks.ModBlocks;
 import net.minecraft.tileentity.TileEntity;
 
 public class ChargingStationTile extends TileEntity {
-    @SuppressWarnings("ConstantConditions")
     public ChargingStationTile() {
         super(ModBlocks.CHARGING_STATION_TILE.get());
     }
